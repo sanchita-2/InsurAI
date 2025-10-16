3e14f5964df2914299ce9826a1d34e05ed387d92
Sanchita-2 <sanchitasinha.1101@gmail.com>
2025-10-16 18:26:35 +0530

    second commit

--- JavaExercises/Datatypes/Datatypes1.java
+++ JavaExercises/Datatypes/Datatypes1.java
@@ -1,10 +1,13 @@
 public class Datatypes1{
     public static void main(String[] args){
-        int A =1410;
+        int A =141025;
         char B= 'b';
     
         boolean D=true;
         double pi =3.4159;
 System.out.println("pi ="+ pi);
+System.out.println("today is "+A);
+System.out.println("i like java programming:" +D);
+System.out.println("what comes after a"+B);
     }
 }
