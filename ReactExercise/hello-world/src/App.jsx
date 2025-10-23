ee4582573e2eb6d1fc91e4e202ffc2f80c9f7100
Sanchita-2 <sanchitasinha.1101@gmail.com>
2025-10-23 18:31:30 +0530

    added react exercise

---
+++ ReactExercise/hello-world/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World 🌍</h1>
+      <h2> Hi Sanchita </h2>
+      <p>Welcome to react+vite journey </p>
+    </div>
+  );
+}
+
+export default App;
